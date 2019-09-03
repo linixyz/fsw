3f86378b9331a54de82d222658cb9b29576c47fd
codinghuang <2812240764@qq.com>
2019-09-03 19:18:14 +0800

    add Coroutine::Socket method

--- include/coroutine_socket.h
+++ include/coroutine_socket.h
@@ -1,6 +1,8 @@
 #ifndef COROUTINE_SOCKET_H
 #define COROUTINE_SOCKET_H
 
+#include "fsw.h"
+
 namespace Fsw { namespace Coroutine {
 class Socket
 {
@@ -9,6 +11,11 @@ private:
 public:
     Socket(int type);
     ~Socket();
+    int accept();
+    ssize_t recv(void *buf, size_t len);
+    ssize_t send(const void *buf, size_t len);
+
+    bool wait_event(int event);
 };
 }
 }
--- src/coroutine/socket.cc
+++ src/coroutine/socket.cc
@@ -5,8 +5,33 @@ using Fsw::Coroutine::Socket;
 
 Socket::Socket(int type)
 {
+    sockfd = fswSocket_create(type);
+    if (sockfd < 0)
+    {
+        return;
+    }
 }
 
 Socket::~Socket()
 {
+}
+
+int Socket::accept()
+{
+    return 0;
+}
+
+ssize_t Socket::recv(void *buf, size_t len)
+{
+    return 0;
+}
+
+ssize_t Socket::send(const void *buf, size_t len)
+{
+    return 0;
+}
+
+bool Socket::wait_event(int event)
+{
+    return 0;
 }
