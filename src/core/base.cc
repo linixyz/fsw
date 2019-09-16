39683b06b150747a9613601258383d4e3a23cd47
codinghuang <2812240764@qq.com>
2019-09-16 22:45:32 +0800

    delete: useless code

--- src/core/base.cc
+++ src/core/base.cc
@@ -71,16 +71,6 @@ int fsw_event_init()
     return 0;
 }
 
-typedef enum
-{
-    UV_CLOCK_PRECISE = 0,  /* Use the highest resolution clock available. */
-    UV_CLOCK_FAST = 1      /* Use the fastest clock with <= 1ms granularity. */
-} uv_clocktype_t;
-
-extern "C" void uv__run_timers(uv_loop_t* loop);
-extern "C" uint64_t uv__hrtime(uv_clocktype_t type);
-extern "C" int uv__next_timeout(const uv_loop_t* loop);
-
 int fsw_event_wait()
 {
     fsw_event_init();
