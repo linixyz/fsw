52a4c8c1aea6a076d06cd3b68e4eb5b7bcb5e153
codinghuang <2812240764@qq.com>
2019-09-29 12:26:14 +0800

    fix: timer example

--- examples/timer.cc
+++ examples/timer.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "fsw/timer.h"
 
-using fsw::TimerManager;
 using fsw::Timer;
+using fsw::timer_manager;
 
 void print_num(void *arg)
 {
@@ -11,17 +11,14 @@ void print_num(void *arg)
 
 int main(int argc, char const *argv[])
 {
-    TimerManager timer_manager;
+    fsw_event_init();
 
     timer_manager.add_timer(1 * Timer::SECOND, print_num, (void*)(uintptr_t)1);
     timer_manager.add_timer(2 * Timer::SECOND, print_num, (void*)(uintptr_t)2);
     timer_manager.add_timer(3 * Timer::SECOND, print_num, (void*)(uintptr_t)3);
     timer_manager.add_timer(4 * Timer::SECOND, print_num, (void*)(uintptr_t)4);
 
-    while (timer_manager.get_next_timeout() > 0)
-    {
-        timer_manager.run_timers();
-    }
+    fsw_event_wait();
 
     return 0;
 }
