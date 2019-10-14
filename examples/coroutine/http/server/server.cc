8cf119e9f72de4b22d836ef2e9a7c70c2e073190
codinghuang <15070967610@163.com>
2019-10-14 20:28:25 +0800

    example: coroutine http server

---
+++ examples/coroutine/http/server/server.cc
@@ -0,0 +1,31 @@
+#include "fsw/coroutine_http_server.h"
+#include "fsw/coroutine.h"
+
+using fsw::Coroutine;
+using fsw::coroutine::HttpServer;
+using fsw::coroutine::Socket;
+
+char response_str[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: 11\r\n\r\nhello world\r\n";
+
+void handler(void *args)
+{
+    return;
+}
+
+int main(int argc, char const *argv[])
+{
+    fsw_event_init();
+
+    Coroutine::create([](void *arg)
+    {
+        char ip[] = "127.0.0.1";
+
+        HttpServer *serv = new HttpServer(ip, 80);
+        serv->set_handler("/index", handler);
+        serv->start();
+    });
+
+    fsw_event_wait();
+
+    return 0;
+}
