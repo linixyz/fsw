b68b6f67add5542e512eb0dabcb049e5034518f6
codinghuang <15070967610@163.com>
2019-10-15 10:05:47 +0800

    fix: example

--- examples/coroutine/http/server/server.cc
+++ examples/coroutine/http/server/server.cc
@@ -2,7 +2,7 @@
 #include "fsw/coroutine.h"
 
 using fsw::Coroutine;
-using fsw::coroutine::HttpServer;
+using fsw::coroutine::http::Server;
 using fsw::coroutine::Socket;
 
 char response_str[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: 11\r\n\r\nhello world\r\n";
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[])
     {
         char ip[] = "127.0.0.1";
 
-        HttpServer *serv = new HttpServer(ip, 80);
+        Server *serv = new Server(ip, 80);
         serv->set_handler("/index", handler);
         serv->start();
     });
