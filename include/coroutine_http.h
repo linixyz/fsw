7ff3d77be767a28e5d5ea279a0562b1a53763493
codinghuang <15070967610@163.com>
2019-10-15 21:45:33 +0800

    delete the useless code

--- include/coroutine_http.h
+++ include/coroutine_http.h
@@ -18,9 +18,6 @@ public:
     int version;
     char *path;
     uint32_t path_len;
-    const char *ext;
-    uint32_t ext_len;
-    uint8_t post_form_urlencoded;
     char *body;
     size_t body_length;
     std::map<char*, char*> header;
