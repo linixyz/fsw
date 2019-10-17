6720f79882d6829d1bcf775989faf2a09f4d3566
codinghuang <15070967610@163.com>
2019-10-17 09:52:09 +0800

    update: http server

--- examples/coroutine/http/server/server.cc
+++ examples/coroutine/http/server/server.cc
@@ -12,13 +12,34 @@ using fsw::Buffer;
 
 void handler(Request *request, Response *response)
 {
+    char header1[] = "Content-Type";
+    Buffer buffer1(12);
+    buffer1.append(header1, 12);
+    char value1[] = "text/html";
+    Buffer buffer2(9);
+    buffer2.append(value1, 9);
+
+    char header2[] = "Connection";
+    Buffer buffer3(10);
+    buffer3.append(value1, 10);
+    char value2[] = "close";
+    Buffer buffer4(5);
+    buffer4.append(value1, 5);
+
+    char header3[] = "Content-Length";
+    Buffer buffer5(14);
+    buffer5.append(value1, 14);
+    char value3[] = "11";
+    Buffer buffer6(2);
+    buffer6.append(value1, 2);
+
     char response_body[] = "hello world";
     Buffer *buffer = new Buffer(1024);
     buffer->append(response_body, sizeof(response_body) - 1);
 
-    response->header["Content-Type"] = "text/html";
-    response->header["Connection"] = "close";
-    response->header["Content-Length"] = std::to_string(buffer->length());
+    response->header[&buffer1] = &buffer2;
+    response->header[&buffer3] = &buffer4;
+    response->header[&buffer5] = &buffer6;
     response->end(buffer);
 
     delete buffer;
