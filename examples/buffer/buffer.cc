da30350f9c67cfd9feeadc9b0b4f82b169967dc7
yangclsir <624372062@qq.com>
2019-10-21 11:29:22 +0800

    测试例子

--- examples/buffer/buffer.cc
+++ examples/buffer/buffer.cc
@@ -10,13 +10,27 @@ int main(int argc, char const *argv[])
     Buffer *buffer1 = new Buffer(1024);
     Buffer *buffer2 = new Buffer(1024);
 
+    //append方法1
     buffer1->append(src_buffer, strlen(src_buffer));
     buffer1->append(src_buffer, strlen(src_buffer));
     assert(strcmp(buffer1->c_buffer(), "aaaa") == 0);
     assert(buffer1->length() == 4);
+
+    //append方法2
     buffer2->append(buffer1);
     buffer2->append(buffer1);
     assert(strcmp(buffer2->c_buffer(), "aaaaaaaa") == 0);
     assert(buffer2->length() == 8);
+
+    Buffer *buffer3 = new Buffer(1024);
+    buffer3->append(src_buffer, strlen(src_buffer));
+
+    //append方法3
+    Buffer *buffer4 = new Buffer(19);
+    std::string s1("This is test string");
+    buffer4->append(s1);
+    assert(buffer4->length() == 19);
+    assert(strcmp(buffer4->c_buffer(),s1.c_str()) == 0);
+
     return 0;
 }
